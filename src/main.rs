54043c41036db0289a65322415e7ae81800e53ac
Nick Hilton <nhilton777@gmail.com>
2022-07-21 00:02:02 -0400

    Use proper imports

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,21 @@
+use windows::{
+  Win32::Foundation::HWND, Win32::UI::Input::KeyboardAndMouse::*, Win32::UI::WindowsAndMessaging::*,
+};
+
 fn main() -> windows::core::Result<()> {
   println!("Hello world!");
 
   unsafe {
     RegisterHotKey(
-      windows::Win32::Foundation::HWND(0),
+      HWND(0),
       1,
-      windows::Win32::UI::Input::KeyboardAndMouse::MOD_WIN
-        | windows::Win32::UI::Input::KeyboardAndMouse::MOD_NOREPEAT,
+      MOD_WIN | MOD_NOREPEAT,
       0xC0, /* the `~ key*/
     );
 
-    let mut msg = windows::Win32::UI::WindowsAndMessaging::MSG::default();
-    while windows::Win32::UI::WindowsAndMessaging::GetMessageA(
-      &mut msg,
-      windows::Win32::Foundation::HWND(0),
-      0,
-      0,
-    )
-    .into()
-    {
-      if msg.message == windows::Win32::UI::WindowsAndMessaging::WM_HOTKEY {
+    let mut msg = MSG::default();
+    while GetMessageA(&mut msg, windows::Win32::Foundation::HWND(0), 0, 0).into() {
+      if msg.message == WM_HOTKEY {
         println!("TEST");
         break;
       }
